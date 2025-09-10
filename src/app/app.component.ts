3822e610806f04819a08e8309a05522dc58d5925
fredLamonica <frederico.lamonica@gmail.com>
2025-09-10 14:31:02 -0300

    Fix missing styles.scss and app.component.ts syntax errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
-@@ .. @@
- import { Component } from '@angular/core';
-+import { Router, NavigationEnd } from '@angular/router';
-+import { filter } from 'rxjs/operators';
- 
- @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
- })
- export class AppComponent {
-   title = 'mktplace-frontend';
-+
-+  constructor(private router: Router) {
-+    // Log de navegação para debug
-+    this.router.events.pipe(
-+      filter(event => event instanceof NavigationEnd)
-+    ).subscribe((event: NavigationEnd) => {
-+      console.log('Navegação para:', event.url);
-+    });
-+  }
- }
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mktplace-frontend';
+
+  constructor(private router: Router) {
+    // Log de navegação para debug
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      console.log('Navegação para:', event.url);
+    });
+  }
+}
